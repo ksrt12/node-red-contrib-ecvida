908c2e1ff61b56fcb0428ec0d7add57b93ed9ef8
Kazakov Stepan <ksrt12group@gmail.com>
2022-03-29 03:16:20 +0300

    Update to 1.0.2
    Move getCookies and getHTML functions to modules
    
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- README.md
+++ README.md
@@ -18,6 +18,10 @@ npm install *full path to cloned repo folder*
 
 ## Changelog
 
+Version 1.0.2 29/03/22:
+
+-   Move getCookies and getHTML functions to modules
+
 Version 1.0.1 28/03/22:
 
 -   Write get counters and acculars logic
--- nodes/ecvida-get.js
+++ nodes/ecvida-get.js
@@ -1,7 +1,6 @@
-const fetch = require("node-fetch");
+const getCookies = require("../lib/getCookies");
+const getHTML = require("../lib/getHTML");
 const sleep = require('util').promisify(setTimeout);
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
 
 module.exports = function (RED) {
 
@@ -27,8 +26,6 @@ module.exports = function (RED) {
             let command = node.command_type;
             let is_debug = node.is_debug;
 
-            const UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36";
-
             const Debug_Log = msg_text => {
                 node.log(msg_text);
                 node.send({ payload: msg_text });
@@ -46,88 +43,21 @@ module.exports = function (RED) {
             };
 
             const formatNumber = str => parseFloat(str.replace(',', '.').replace(new RegExp(/\s/, 'g'), ''));
-
+            const is = str => (str && str.length > 700);
             const cleanStatus = () => node.status({});
 
             async function make_action() {
 
                 cleanStatus();
 
-                let is_error = false;
-                let is_cookies = cookies.length > 700;
-
-                if (!is_cookies) {
-
-                    const addCookies = res => {
-                        let raw = res.headers.raw()['set-cookie'];
-                        if (raw) {
-                            raw.forEach(tmp => {
-                                let tmp_cookie = tmp.substring(0, tmp.indexOf('; ')) + ";";
-                                if (tmp_cookie.length > 200) {
-                                    cookies += tmp_cookie;
-                                }
-                            });
-                            is_cookies = true;
-                        } else {
-                            is_cookies = false;
-                            throw "Invalid login/password";
-                        }
-                    };
-
-                    setStatus("blue", "ring", "Get cookies", "ASPXAUTH");
-                    await fetch("https://lkabinet.online/login/enterPassword?emailOrPhone=" + username + "&accountType=",
-                        {
-                            method: "POST",
-                            body: "EmailOrPhone=" + username + "&Password=" + encodeURIComponent(password),
-                            headers: { 'User-Agent': UserAgent, 'Content-Type': 'application/x-www-form-urlencoded' },
-                            redirect: 'manual'
-                        })
-                        .then(addCookies)
-                        .catch(err => SetError("Get cookies: ASPXAUTH", err));
-
-                    if (is_cookies) {
-
-                        setStatus("green", "ring", "Get cookies", "ASPXROLES");
-                        await fetch("https://lkabinet.online/",
-                            {
-                                method: "GET",
-                                headers: { 'User-Agent': UserAgent, 'Content-Type': 'application/x-www-form-urlencoded', 'cookie': cookies },
-                                redirect: 'manual'
-                            })
-                            .then(addCookies)
-                            .catch(err => SetError("Get cookies: ASPXROLES", err));
-
-                        if (cookies.length > 700) {
-                            setStatus("blue", "dot", "Get cookies", "OK");
-                            Debug_Log("Copy cookies to ecvida-login node:");
-                            Debug_Log(cookies);
-                            is_cookies = true;
-                        }
-                    }
+                if (!is(cookies)) {
+                    cookies = await getCookies(username, password, setStatus, SetError, Debug_Log);
                 }
 
-                if (is_cookies) {
+                if (is(cookies)) {
 
                     let topic = "Get " + command;
-                    const fetchGet = async url => {
-
-                        let out;
-                        setStatus("blue", "ring", topic, "begin");
-                        await fetch(url,
-                            {
-                                method: "GET",
-                                headers: { 'User-Agent': UserAgent, 'Content-Type': 'application/x-www-form-urlencoded', 'cookie': cookies },
-                                redirect: 'manual'
-                            })
-                            .then(res => res.text())
-                            .then(text => {
-                                let { document } = (new JSDOM(text)).window;
-                                out = document;
-                            })
-                            .catch(err => SetError("Get " + command, err));
-                        return out;
-                    };
-
+                    setStatus("blue", "ring", topic, "begin");
                     let out;
                     if (command === "counters") {
 
@@ -135,13 +65,14 @@ module.exports = function (RED) {
                         let curr_date = new Date();
                         let curr_date_str = `01.${curr_date.getMonth()}.${curr_date.getFullYear()}`;
 
-                        let document = await fetchGet("https://lkabinet.online/Counters/GetValues?DayToString=" + curr_date_str);
+                        let document = await getHTML("https://lkabinet.online/Counters/GetValues?DayToString=" + curr_date_str, topic, cookies, SetError);
                         let all = document.querySelectorAll("body > form > div.indications_list > div");
 
                         for (let counter of all) {
                             let id = counter.getAttribute("data-id");
                             let content = counter.querySelector("div.content");
                             let title = content.querySelector("div.title").textContent;
+                            let serial = counter.querySelector("div.serial").textContent.slice(16);
                             let vals = [];
                             content.querySelectorAll("div.cells_cover > div.cell").forEach(cell => {
                                 vals.push(formatNumber(cell.querySelector("span").textContent));
@@ -151,7 +82,7 @@ module.exports = function (RED) {
 
                         out = counters;
                     } else {
-                        let document = await fetchGet("https://lkabinet.online/accruals");
+                        let document = await getHTML("https://lkabinet.online/accruals", topic, cookies, SetError);
 
                         let month = document.querySelector("#placeForShowAccrual > div.page_title.accrualTitle > h2").textContent;
                         let sum = formatNumber(document.querySelector("#accrualPage > div.payment_right > div.accruals_total.accrualTotal > div.accr_head > div > div.prise").textContent);
